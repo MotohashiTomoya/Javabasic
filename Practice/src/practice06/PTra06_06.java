bb004c02c9f765088e08f32e1ac14df419bc9f24
MotohashiTomoya <MotohashiTomoya@users.noreply.github.com>
2019-02-04 15:51:33 +0900

    6-7

--- Practice/src/practice06/PTra06_06.java
+++ Practice/src/practice06/PTra06_06.java
@@ -20,5 +20,30 @@ public class PTra06_06 {
 		 * ■■■■□
 		 */
 
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("■");
+			}
+			for (int j = 5; j > i; j--) {
+				System.out.print("□");
+			}
+			System.out.println();
+		}
+
+//		 for (int i =1 ; i <= 5; i++) {
+//	            for (int j = 1; j <= 5; j++) {
+//	            	if (i==5&&j==1||i==5&&j==2||
+//	            			i==5&&j==3||i==5&&j==4||
+//	            			i==4&&j==1||i==4&&j==2||
+//	            			i==4&&j==3||i==3&&j==1||
+//	            			i==3&&j==2||i==2&&j==1){
+//	            		System.out.print("■");
+//	            	} else {
+//	            		System.out.print("□");
+//	            	}
+//	            }
+//	            System.out.println();
+//	        }
+
 	}
 }
