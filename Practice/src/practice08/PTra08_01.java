e2b456cd9939b22ec287989cf352cad95109a52a
MotohashiTomoya <MotohashiTomoya@users.noreply.github.com>
2019-02-07 16:26:06 +0900

    8-6

--- Practice/src/practice08/PTra08_01.java
+++ Practice/src/practice08/PTra08_01.java
@@ -1,4 +1,5 @@
 package practice08;
+
 /*
  * PTra08_01.java
  *   作成	LIKEIT	2017
@@ -10,10 +11,10 @@ public class PTra08_01 {
 	public static void main(String[] args) {
 
 		// ★ 変数receiveを宣言し、メソッドnumberを実行した結果を代入してください
-
+		int receive=number();
 
 		// ★ 変数receiveを出力してください
-
+		System.out.println(receive);
 	}
 
 	/*
